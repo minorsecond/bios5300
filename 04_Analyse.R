76a646945f7e2452e2e292da0b9814c168eaec51
Ross Wardrup <rwardrup@Rosss-iMac.local>
2017-11-03 13:21:14 -0500

    Created stroke models

--- 04_Analyse.R
+++ 04_Analyse.R
@@ -16,21 +16,23 @@ readRDS("Data/transformed.rds") -> brfss.survey.design
 
 # Models ----
 # First one here is standard binomial model for surveys
-stroke.binom <- svyglm(CVDSTRK3 ~ RACE.ETH + AGE.GROUP + INCOME2 + SEX + 
-                                     EDUCA + MARITAL + N.CHILDREN + EXERANY2 +
-                                     ADDEPEV2 + HEAVY.DRINKER + SMOKER.STATUS + MARIJANA.5GRP +
-                                     STATE,
-                                   brfss.survey.design, family = "binomial")
+results$stroke.binom <- svyglm(CVDSTRK3 ~ HEAVY.DRINKER + SMOKER.STATUS + BINGE.DRINKER +
+                                 DAYS.ANXIOUS + DAYS.ENERGETIC + MENTHLTH + GOOD.HEALTH +
+                                 ADDEPEV2 + SLEPTIM1 + EXERANY2 + GENHLTH + N.CHILDREN +
+                                 EDUCA + INCOME2 + RACE.ETH + HIGH.RISK + DAYS.ANXIOUS +
+                                 STATE,
+                               brfss.survey.design, family = "binomial")
 
 # Check phi to determine if we need to use the quasibinomial method (adds a dispersion paramter)
-phi.b <- sum(resid(satisfied.surveylogistic.b, type = "pearson")^2) / df.residual(satisfied.surveylogistic.b)
+results$phi.b <- sum(resid(results$stroke.binom, type = "pearson")^2) / df.residual(results$stroke.binom)
 
 # Quasibinomial method to use if phi is too large
-satisfied.surveylogistic.qb <- svyglm(CVDSTRK3 ~ RACE.ETH + AGE.GROUP + INCOME2 + SEX + 
-                                     EDUCA + MARITAL + N.CHILDREN + EXERANY2 +
-                                     ADDEPEV2 + HEAVY.DRINKER + SMOKER.STATUS + MARIJANA.5GRP +
-                                     STATE,
-                                   brfss.survey.design, family = "quasibinomial")
+results$satisfied.surveylogistic.qb <- svyglm(CVDSTRK3 ~ HEAVY.DRINKER + SMOKER.STATUS + BINGE.DRINKER +
+                                                DAYS.ANXIOUS + DAYS.ENERGETIC + MENTHLTH + GOOD.HEALTH +
+                                                ADDEPEV2 + SLEPTIM1 + EXERANY2 + GENHLTH + N.CHILDREN +
+                                                EDUCA + INCOME2 + RACE.ETH + HIGH.RISK + DAYS.ANXIOUS +
+                                                STATE,
+                                              brfss.survey.design, family = "quasibinomial")
 
 # Tables ----
 
