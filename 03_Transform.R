657b166a46a2a1ac4f7548582661dad0bfe8fd9c
Ross Wardrup <rwardrup@Rosss-iMac.local>
2017-11-05 04:45:17 -0600

    Big changes. unfortunately didn't keep track of them all :(

--- 03_Transform.R
+++ 03_Transform.R
@@ -7,11 +7,9 @@ library(classInt)
 library(dplyr)
 library(survey)
 library(srvyr)
+library(Amelia)
 source("./Functions/tukey.R")
 options(survey.lonely.psu = "certainty")
-
-# Prompt to remove outliers. I did in my tests
-outlierKD(brfss, X_BMI5)
 
 # Add state names to DF
 state.codes$X_STATE <- state.codes$VALUE
@@ -29,94 +27,110 @@ brfss.survey1 <- brfss.survey1[ , -which(names(brfss.survey1) %in% c("X_STATE", 
 
 # Recode the BRFSS variables according to the codebook.
 brfss.survey1 <- brfss.survey1 %>%
-  mutate(IDATE = as.Date(IDATE, "%m%d%Y"),
-         RACE.ETH = car::recode(X_RACEGR3, "1='White only, Non-Hispanic'; 2='Black only, Non-Hispanic'; 3='Other race only, Non-Hispanic'; 4='Multiracial, Non-Hispanic'; 5='Hispanic'; 9='DK/US/Refused'"),
-         RACE.ETH = as.factor(RACE.ETH),
-         AGE.GROUP = car::recode(X_AGE_G, "1='Age 18 to 24'; 2='Age 25 to 34'; 3='Age 35 to 44'; 4='Age 45 to 54'; 5='Age 55 to 64'; 6='Age 65 or older'"),
-         AGE.GROUP = as.factor(AGE.GROUP),
-         INCOME2 = car::recode(INCOME2, "1='<$10,000'; 2='<$15,000'; 3='<$20,000'; 4='<$25,000'; 5='<$35,000'; 6='<$50,000'; 7='<$75,000'; 8='>$75,000'; 77='DK/NS'; 99='Refused'"),
-         INCOME2 = as.factor(INCOME2),
-         SEX = car::recode(SEX, "1='Male'; 2='Female'; 9='NA'"),
-         SEX = as.factor(SEX),
-         X_BMI5 = X_BMI5 / 100,
-         EDUCA = car::recode(EDUCA, "1='Never attended school or only kindergarten'; 2='Grades 1 through 8 (Elementary) '; 3='Grades 9 through 11 (Some high school) '; 4='Grade 12 or GED (High school graduate) '; 5='College 1 year to 3 years (Some college or technical school)'; 6='College 4 years or more (College graduate) '; 9='Refused'"),
-         EDUCA = as.factor(EDUCA),
-         N.CHILDREN = car::recode(X_CHLDCNT, "1='No children in household'; 2='One child in household'; 3='Two children in household'; 4='Three children in household'; 5='Four children in household'; 6='Five or more children in household'; 9='DK/NS/Missing'"),
-         N.CHILDREN = as.factor(N.CHILDREN),
-         GENHLTH = car::recode(GENHLTH, "1='Excellent'; 2='Very good'; 3='Good'; 4='Fair'; 5='Poor'; 7='DK/NS'; 9='Refused'"),
-         GENHTL = as.factor(GENHLTH),
-         EXERCISE = car::recode(EXERANY2, "1=1; 2=0; 7=NA; 9=NA"),
-         EXERCISE = as.integer(EXERCISE),
-         DIFFWALK = car::recode(DIFFWALK, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         DIFFWALK = as.factor(DIFFWALK),
-         DIFFDRES = car::recode(DIFFDRES, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         DIFFDRES = as.factor(DIFFDRES),
-         DECIDE = car::recode(DECIDE, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         DECIDE = as.factor(DECIDE),
-         DIFFALON = car::recode(DIFFALON, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         DIFFALON = as.factor(DIFFALON),
-         EXERANY2 = car::recode(EXERANY2, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         EXERANY2 = as.factor(EXERANY2),
-         SLEPTIM1 = car::recode(SLEPTIM1, "77='DK/NS'; 99 ='Refused'"),
-         ADDEPEV2 = car::recode(ADDEPEV2, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         ADDEPEV2 = as.factor(ADDEPEV2),
-         SATISFIED = car::recode(LSATISFY, "c(1,2)=1; c(3,4)=0; NA=NA; else=NA"),  # Variable for logistic reg
-         SATISFIED = as.factor(SATISFIED),
-         LSATISFY = car::recode(LSATISFY, "1='Very satisfied'; 2='Satisfied'; 3='Dissatisfied'; 4='Very dissatisfied'; 7='DK/NS'; 9='Refused'"),
-         LSATISFY = as.factor(LSATISFY),
-         GOOD.HEALTH = car::recode(X_RFHLTH, "1='Good or better health'; 2='Fair or poor health'; 9='DK/NS/Refused/Missing'"),
-         GOOD.HEALTH = as.factor(GOOD.HEALTH),
-         MENTHLTH = car::recode(MENTHLTH, "88=0; 99=NA; 77=NA"),
-         MENTHLTH = as.numeric(MENTHLTH),
-         DAYS.ENERGETIC = car::recode(QLHLTH2, "88='0'; 77='DK/NS'; 99='Refused'"),
-         DAYS.ANXIOUS = car::recode(QLSTRES2, "88='0'; 77='DK/NS'; 99='Refused'"),
-         DAYS.SAD = car::recode(QLMENTL2, "88='0'; 77='DK/NS'; 99='Refused'"),
-         DAYS.PHYS.PREVENTED = car::recode(PAINACT2, "88='0'; 77='DK/NS'; 99='Refused'"),
-         CVDSTRK3 = car::recode(CVDSTRK3, "1=1; 2=0; 7=NA; 9=NA"),
-         CVDSTRK3 = as.factor(CVDSTRK3),
-         MARITAL = car::recode(MARITAL, "1='Married'; 2='Divorced'; 3='Widowed'; 4='Separated'; 5='Never married'; 6='A member of an unmarried couple'; 9='Refused'"),
-         MARITAL = as.factor(MARITAL),
-         MARIJANA = car::recode(MARIJANA, "77='DK/NS'; 88='0'; 99='Refused'"),
-         BINGE.DRINKER = car::recode(X_RFBING5, "1='Yyes'; 2='No'; 9='DK/NS/Refused/Missing'"),
-         BINGE.DRINKER = as.factor(BINGE.DRINKER),
-         N.ALC.DRINKS.WEEK = car::recode(X_DRNKWEK, "99900='DK/US/Refused/Missing'"),
-         HEAVY.DRINKER = car::recode(X_RFDRHV5, "1='No'; 2='Yes'; 9='DK/US/Refused/Missing'"),
-         HEAVY.DRINKER = as.factor(HEAVY.DRINKER),
-         SMOKER.STATUS = car::recode(X_SMOKER3, "1 = 'Current smoker - now smokes every day'; 2='Current smoker - now smokes some days'; 3='Former smoker'; 4='Never smoked'; 9='DK/Refused/Missing'"),
-         SMOKER.STATUS = as.factor(SMOKER.STATUS),
-         SMOKE100 = car::recode(SMOKE100, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         SMOKE100 = as.factor(SMOKE100),
-         HIGH.RISK = car::recode(HIVRISK4, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
-         HIGH.RISK = as.factor(HIGH.RISK),
-         DRINK.DRIVE = car::recode(X_DRNKDRV, "1='Have driven after having too much to drink'; 2='Have not driven after having too much to drink'; 9='DK/NS/Refused/Missing'"),
-         DRINK.DRIVE = as.factor(DRINK.DRIVE))
+  mutate(interview.date = as.Date(IDATE, "%m%d%Y"),
+         race.eth = car::recode(X_RACEGR3, "1='White only, Non-Hispanic'; 2='Black only, Non-Hispanic'; 3='Other race only, Non-Hispanic'; 4='Multiracial, Non-Hispanic'; 5='Hispanic'; 9=NA"),
+         race.eth = as.factor(race.eth),
+         age.grp = car::recode(X_AGE_G, "1='Age 18 to 24'; 2='Age 25 to 34'; 3='Age 35 to 44'; 4='Age 45 to 54'; 5='Age 55 to 64'; 6='Age 65 or older'"),
+         age.grp = as.factor(age.grp),
+         income = car::recode(INCOME2, "1='<$10,000'; 2='<$15,000'; 3='<$20,000'; 4='<$25,000'; 5='<$35,000'; 6='<$50,000'; 7='<$75,000'; 8='>$75,000'; 77='DK/NS'; 99='Refused'"),
+         income = as.factor(income),
+         sex = car::recode(SEX, "1='Male'; 2='Female'; 9=NA"),
+         sex = as.factor(sex),
+         bmi = X_BMI5 / 100,
+         edu.level = car::recode(EDUCA, "1='Never attended school or only kindergarten'; 2='Grades 1 through 8 (Elementary) '; 3='Grades 9 through 11 (Some high school) '; 4='Grade 12 or GED (High school graduate) '; 5='College 1 year to 3 years (Some college or technical school)'; 6='College 4 years or more (College graduate) '; 9='Refused'"),
+         edu.level = as.factor(edu.level),
+         n.children = car::recode(X_CHLDCNT, "1='No children in household'; 2='One child in household'; 3='Two children in household'; 4='Three children in household'; 5='Four children in household'; 6='Five or more children in household'; 9=NA"),
+         n.children = as.factor(n.children),
+         self.hlth.level = car::recode(GENHLTH, "1='Excellent'; 2='Very good'; 3='Good'; 4='Fair'; 5='Poor'; 7='DK/NS'; 9='Refused'"),
+         self.hlth.level = as.factor(self.hlth.level),
+         exercises.binary = car::recode(EXERANY2, "1=1; 2=0; 7=NA; 9=NA"),
+         exercises.binary = as.integer(exercises.binary),
+         diff.walking = car::recode(DIFFWALK, "1='Yes'; 2='No'; 7='DK/NS'; 9=NA"),
+         diff.walking = as.factor(diff.walking),
+         diff.dressing = car::recode(DIFFDRES, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
+         diff.dressing = as.factor(diff.dressing),
+         diff.decisions = car::recode(DECIDE, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
+         diff.decisions = as.factor(diff.decisions),
+         diff.errands.alone = car::recode(DIFFALON, "1='Yes'; 2='No'; 7='DK/NS'; 9=NA"),
+         diff.errands.alone = as.factor(diff.errands.alone),
+         exercises.yn = car::recode(EXERANY2, "1='Yes'; 2='No'; 7=NA; 9=NA"),
+         exercises.yn = as.factor(exercises.yn),
+         daily.sleep.hrs = car::recode(SLEPTIM1, "77='DK/NS'; 99 =NA"),
+         diag.depression = car::recode(ADDEPEV2, "1='Yes'; 2='No'; 7='DK/NS'; 9=NA"),
+         diag.depression = as.factor(diag.depression),
+         life.satisfied.binary = car::recode(LSATISFY, "c(1,2)=1; c(3,4)=0; NA=NA; else=NA"),  # Variable for logistic reg
+         life.satisfied.binary = as.factor(life.satisfied.binary),
+         life.satisfied = car::recode(LSATISFY, "1='Very satisfied'; 2='Satisfied'; 3='Dissatisfied'; 4='Very dissatisfied'; 7='DK/NS'; 9='Refused'"),
+         life.satisfied = as.factor(life.satisfied),
+         good.health = car::recode(X_RFHLTH, "1='Good or better health'; 2='Fair or poor health'; 9='DK/NS/Refused/Missing'"),
+         good.health = as.factor(good.health),
+         days.poor.mental.health = car::recode(MENTHLTH, "88=0; 99=NA; 77=NA"),
+         days.poor.mental.health = as.numeric(days.poor.mental.health),
+         days.energetic = car::recode(QLHLTH2, "88='0'; 77='DK/NS'; 99='Refused'"),
+         days.anxious = car::recode(QLSTRES2, "88='0'; 77=NA; 99=NA"),
+         days.anxious <- as.integer(days.anxious),
+         days.sad = car::recode(QLMENTL2, "88='0'; 77=NA; 99=NA"),
+         days.sad <- as.integer(days.sad),
+         days.pain.prev.phys.act = car::recode(PAINACT2, "88='0'; 77='DK/NS'; 99='Refused'"),
+         diag.stroke = car::recode(CVDSTRK3, "1=1; 2=0; 7=NA; 9=NA"),
+         diag.stroke = as.factor(diag.stroke),
+         marital.status = car::recode(MARITAL, "1='Married'; 2='Divorced'; 3='Widowed'; 4='Separated'; 5='Never married'; 6='A member of an unmarried couple'; 9='Refused'"),
+         marital.status = as.factor(marital.status),
+         veteran = car::recode(VETERAN3, "1='Yes'; 2='No'; 7=NA; 9=NA"),
+         veteran = as.factor(veteran),
+         days.use.marij = car::recode(MARIJANA, "77='DK/NS'; 88='0'; 99='Refused'"),
+         alc.binge = car::recode(X_RFBING5, "1='Yyes'; 2='No'; 9='DK/NS/Refused/Missing'"),
+         alc.binge = as.factor(alc.binge),
+         alc.n.drinks.weekly = car::recode(X_DRNKWEK, "99900=NA"),
+         alc.heavy.drinker = car::recode(X_RFDRHV5, "1='No'; 2='Yes'; 9=NA"),
+         alc.heavy.drinker = as.factor(alc.heavy.drinker),
+         smoker.status = car::recode(X_SMOKER3, "1 = 'Current smoker - now smokes every day'; 2='Currentsmoker - now smokes some days'; 3='Former smoker'; 4='Never smoked'; 9=NA"),
+         smoker.status = as.factor(smoker.status),
+         smoker.100.cigs.life = car::recode(SMOKE100, "1='Yes'; 2='No'; 7='DK/NS'; 9=NA"),
+         smoker.100.cigs.life = as.factor(smoker.100.cigs.life),
+         risky.behavior = car::recode(HIVRISK4, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
+         risky.behavior = as.factor(risky.behavior),
+         diag.nonskin.cancer = car::recode(CHCOCNCR, "1='Yes'; 2='No'; 7='DK/NS'; 9='Refused'"),
+         diag.nonskin.cancer = as.factor(diag.nonskin.cancer))
 
 # Only keep the variables we need
-brfss.survey1 <- brfss.survey1[, c("X_LLCPWT", "X_STSTR", "X_PSU", "STATE", "IDATE", "RACE.ETH", "AGE.GROUP", "INCOME2", "SEX", "X_BMI5", "EDUCA", "N.CHILDREN","GENHLTH", "EXERCISE",
-                                   "EXERANY2", "SLEPTIM1", "ADDEPEV2","SATISFIED", "LSATISFY", "GOOD.HEALTH", "MENTHLTH", "DAYS.ENERGETIC", "DAYS.ANXIOUS", "DAYS.SAD",
-                                   "DAYS.PHYS.PREVENTED", "CVDSTRK3","MARITAL", "MARIJANA", "BINGE.DRINKER", "N.ALC.DRINKS.WEEK", "HEAVY.DRINKER", "SMOKER.STATUS", "HIGH.RISK", 
-                                   "DRINK.DRIVE")]
+brfss.survey1 <- brfss.survey1[, c("X_LLCPWT", "X_STSTR", "X_PSU", "STATE", "interview.date", 
+                                   "race.eth", "age.grp", "income", "sex", "bmi", "edu.level", 
+                                   "n.children", "exercises.yn", "daily.sleep.hrs", "diag.depression", "diag.stroke",
+                                   "days.poor.mental.health", "days.anxious", "days.sad", "days.pain.prev.phys.act",
+                                   "marital.status", "alc.binge", "alc.n.drinks.weekly",
+                                   "alc.heavy.drinker", "smoker.status", "smoker.100.cigs.life",
+                                   "veteran")]
+
+# Prompt to remove outliers. I did in my tests
+outlierKD(brfss.survey1, bmi)
+outlierKD(brfss.survey1, alc.n.drinks.weekly)
+outlierKD(brfss.survey1, as.integer(daily.sleep.hrs))
+
+
 # Split variables into 5 groups using kmeans (faster than Jenks)
-seed=5
-breaks <- classIntervals(as.numeric(brfss.survey1$MARIJANA), n = 5, style="kmeans")$brks
-brfss.survey1$MARIJANA.5GRP <- as.factor(cut(as.numeric(brfss.survey1$MARIJANA), breaks=breaks, labels = as.character(1:5)))
+#seed=5
+#breaks <- classIntervals(as.numeric(brfss.survey1$MARIJANA), n = 5, style="kmeans")$brks
+#brfss.survey1$MARIJANA.5GRP <- as.factor(cut(as.numeric(brfss.survey1$MARIJANA), breaks=breaks, labels = as.character(1:5)))
 
 
 # Flush memory
 gc()
 
 # Create the survey design
+#brfss.survey2 <- subset(brfss.survey1, diag.depression != "Refused")
 brfss.survey.design <- svydesign(
   nest = T,
   ids=~X_PSU, 
   strata=~X_STSTR, 
   weights=~X_LLCPWT, 
-  data = brfss.survey1)
+  data = brfss.survey1
+)
+
+
 
 #  Don't need these now. Save RAM.
 rm(brfss.survey1)
-rm(seed)
-rm(breaks)
 
 # Save the transformed data ----
 saveRDS(brfss.survey.design, "Data/transformed.rds")
--- 04_Analyse.R
+++ 04_Analyse.R
@@ -2,6 +2,8 @@
 # library(broom)
 # library(modelr)
 library(ggplot2)
+library(srvyr)
+library(survey)
 
 #Disable scientific notation
 options(scipen=999)
@@ -15,11 +17,26 @@ readRDS("Data/transformed.rds") -> brfss.survey.design
 # Specific transformations ----
 
 # Models ----
+# Relevel some of the factors so that the regression output is easier to interpret.
+brfss.survey.design$variables$sex <- relevel(brfss.survey.design$variables$sex, ref = "Male")
+brfss.survey.design$variables$smoker.status <- relevel(brfss.survey.design$variables$smoker.status, ref = "Never smoked")
+brfss.survey.design$variables$n.children <- relevel(brfss.survey.design$variables$n.children, ref = "No children in household")
+brfss.survey.design$variables$diag.depression <- relevel(brfss.survey.design$variables$diag.depression, ref = "No")
+brfss.survey.design$variables$smoker.status <- relevel(brfss.survey.design$variables$smoker.status, ref = "Never smoked")
+brfss.survey.design$variables$diff.walking <- relevel(brfss.survey.design$variables$diff.walking, ref = "No")
+brfss.survey.design$variables$diff.errands.alone <- relevel(brfss.survey.design$variables$diff.errands.alone, ref = "No")
+brfss.survey.design$variables$diff.walking <- relevel(brfss.survey.design$variables$diff.walking, ref = "No")
+brfss.survey.design$variables$race.eth <- relevel(brfss.survey.design$variables$race.eth, ref = "Other race only, Non-Hispanic")
+brfss.survey.design$variables$age.grp <- relevel(brfss.survey.design$variables$age.grp, ref = "Age 18 to 24")
+brfss.survey.design$variables$exercises.yn <- relevel(brfss.survey.design$variables$exercises.yn, ref = "No")
+brfss.survey.design$variables$veteran <- relevel(brfss.survey.design$variables$veteran, ref = "No")
+
 # First one here is standard binomial model for surveys
-results$stroke.binom <- svyglm(CVDSTRK3 ~ HEAVY.DRINKER + SMOKER.STATUS + BINGE.DRINKER +
-                                 as.integer(DAYS.ANXIOUS) + as.integer(DAYS.ENERGETIC) +  + as.integer(DAYS.ANXIOUS) + 
-                                 as.integer(MENTHLTH) + GOOD.HEALTH + ADDEPEV2 + as.integer(SLEPTIM1) + EXERCISE + 
-                                 GENHLTH + RACE.ETH + HIGH.RISK + AGE.GROUP + X_BMI5 + STATE,
+results$stroke.binom <- svyglm(diag.stroke ~ alc.heavy.drinker + smoker.status + n.children +
+                                 days.anxious + days.sad + 
+                                 days.poor.mental.health + diag.depression + 
+                                 daily.sleep.hrs + exercises.yn + bmi + 
+                                 sex + veteran + race.eth + age.grp,
                                brfss.survey.design, family = "binomial")
 
 # Calculate OR and 95% CI for estimate
@@ -29,23 +46,34 @@ results$stroke.binom.ORCI <- exp(cbind(OR = coef(results$stroke.binom), confint(
 results$phi.b <- sum(resid(results$stroke.binom, type = "pearson")^2) / df.residual(results$stroke.binom)
 
 # Quasibinomial method to use if phi is too large
-results$stroke.qbinom <- svyglm(CVDSTRK3 ~ HEAVY.DRINKER + SMOKER.STATUS +
-                                  as.integer(DAYS.ANXIOUS) + as.integer(DAYS.ENERGETIC) + as.integer(DAYS.ANXIOUS) + 
-                                  as.integer(MENTHLTH) + ADDEPEV2 + as.integer(SLEPTIM1) + as.factor(EXERCISE) + 
-                                  RACE.ETH + AGE.GROUP + X_BMI5,
+
+results$stroke.qbinom <- svyglm(diag.stroke ~ alc.heavy.drinker + smoker.status + n.children +
+                                  days.anxious + days.sad +  days.poor.mental.health +
+                                  diag.depression + daily.sleep.hrs + exercises.yn + bmi + 
+                                  sex + veteran + race.eth + age.grp,
                                 brfss.survey.design, family = "quasibinomial")
+
 # Calculate OR and 95% CI for estimate
 results$stroke.qb.ORCI <- exp(cbind(OR = coef(results$stroke.qbinom), confint(results$stroke.qbinom)))
+
+#KW test to determine if men/women have more strokes
+svyranktest(CVDSTRK3 ~ SEX, design = brfss.survey.design)
 
 # Tables ----
 
 results$stroke.exercise <- svytable(~CVDSTRK3 + EXERCISE, design = brfss.survey.design)
 colnames(results$stroke.exercise) <- c("No exercise in past 30 days", "Exercised in past 30 days")
-rownames(results$stroke.exercise) <- c("Stroke diagnosis", "No stroke diagnosis")
+rownames(results$stroke.exercise) <- c("No stroke diagnosis", "Stroke diagnosis")
 results$stroke.exercise.p <- prop.table(results$stroke.exercise, 1)
 #results$stroke.exercise.chisq <- svychisq(~CVDSTRK3 + EXERCISE, design = brfss.survey.design)
 
+results$stroke.sex <- na.omit(svytable(~diag.stroke + sex, design = brfss.survey.design))
+colnames(results$stroke.sex) <- c("Male", "Female")
+rownames(results$stroke.sex) <- c("No stroke diagnosis", "Stroke diagnosis")
+results$stroke.sex.p <- prop.table(results$stroke.sex, 1)
+
 # Plots ----
+
 
 # Save the results object ----
 saveRDS(results, file = "Data/results.rds")
