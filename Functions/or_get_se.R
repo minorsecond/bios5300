95f7a3435ac03fa9a2990ef439205c5f97aa0165
Ross Wardrup <rwardrup@Rosss-iMac.local>
2017-11-10 16:48:55 -0600

    Created detachall and or_get_se functions.

---
+++ Functions/or_get_se.R
@@ -0,0 +1,7 @@
+get.or.se <- function(model) {
+  broom::tidy(model) %>% 
+    mutate(or = exp(estimate),
+           var.diag = diag(vcov(model)),
+           or.se = sqrt(or^2 * var.diag)) %>%
+    select(or.se) %>% unlist %>% unname
+}
